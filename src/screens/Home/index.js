df5131afe9b796a7439a68385a3c0c1be744ae0b
Michael Chan <me@michchan.com>
2017-12-08 00:37:37 +0800

    add boilerplate screens

---
+++ src/screens/Home/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export const HomeScreen = props => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>this is a Home screen</Text>
+    </View>
+);
+
+export default HomeScreen;
---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export const SignInScreen = props => (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>this is a Sign In screen</Text>
+    </View>
+);
+
+export default SignInScreen;
